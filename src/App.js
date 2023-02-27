25f23918189dc64096b4844015b7334d29054393
DJGValls <jiga1980@gmail.com>
2023-02-27 16:23:20 +0100

    fist iteration

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="title">
+        <section id="title">
+          <h1>Say hello to ReactJS</h1>
+          <p>You will learn how <br/>the most popular frontend library <br />and become a super Ninja developer</p>
+
+        </section>
+
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
